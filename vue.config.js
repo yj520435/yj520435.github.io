50b6107094c5208d83c198c6bcb08d34442a76ee
yujin <yjkim@thecnp.co.kr>
2023-09-16 01:34:58 +0900

    rebuild

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: 'https://yj520435.github.io/',
   outputDir: 'docs'
 })
