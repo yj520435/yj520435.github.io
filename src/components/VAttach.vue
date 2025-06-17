0e54978d58d6c2fbe79ffd5338a90bfc9b7b3ac3
yujin <yj520435@gmail.com>
2025-06-17 15:29:25 +0900

    이력서, 경럭기술서 링크 컴포넌트 추가

---
+++ src/components/VAttach.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import VItem from './base/VItem.vue';
+import VWrapper from './base/VWrapper.vue';
+
+async function open(id: string) {
+  window.open(`https://drive.google.com/file/d/${id}/view?usp=drive_link`, '_blank');
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <VWrapper id="resume">
+      <div class="view">
+        <VItem
+          icon="attachment"
+          @click="open('1GIeAFzsrG0-7VTfHsxE3J--ACLu2KVRF')"
+        >
+          <template #text>
+            <span>이력서</span>
+          </template>
+        </VItem>
+      </div>
+    </VWrapper>
+    <VWrapper id="career">
+      <div class="view">
+        <VItem
+          icon="attachment"
+          @click="open('13u9Y8Mq-y6ym-Wy1LaBwMq_vOsKHzeIi')"
+        >
+          <template #text>
+            <span>경력기술서</span>
+          </template>
+        </VItem>
+      </div>
+    </VWrapper>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  height: fit-content;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 24px;
+
+  :deep(.text) {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+}
+</style>
